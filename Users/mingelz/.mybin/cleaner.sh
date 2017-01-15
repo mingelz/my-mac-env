b446de0b5cc9c27e79d867e3f580838f6af36441
mingelz <mingelz@gmail.com>
2017-01-15 21:30:41 +0800

    add cleaner script

--- Users/mingelz/.mybin/cleaner.sh
+++ Users/mingelz/.mybin/cleaner.sh
@@ -1,3 +1,8 @@
+if [ `whoami` == "root" ]; then
+  echo "\033[31mWARNING: Do NOT run as root!\033[0m"
+  exit 1
+fi
+
 # clean .DS_Store
 sudo find / -name ".DS_Store" -depth -exec rm {} \;
 
@@ -12,3 +17,16 @@ mkdir ~/.vim/undo
 # shell history
 rm -rf ~/.bash_history
 rm -rf ~/.zsh_history
+
+# user home
+rm -rf ~/.npm_updater.json
+rm -rf ~/.npminstall_tarball
+rm -rf ~/.oracle_jre_usage
+rm -rf ~/.subversion
+rm -rf ~/.tnpm_tmp
+rm -rf ~/.v8flags*
+rm -rf ~/.zcompdump-*
+
+# brew
+brew cleanup -n
+brew prune -n
